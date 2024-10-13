db35129999340986885c5a45a890a1d14a357479
Øystein Walle <oystwa@gmail.com>
2024-10-13 18:39:13 +0200

    Only switch dir for preset

--- lua/cmake-tools/init.lua
+++ lua/cmake-tools/init.lua
@@ -291,15 +291,18 @@ function cmake.build(opt, callback)
   end
 
   local args
+  local cwd
   local presets_exists = config.base_settings.use_preset and Presets.exists(config.cwd)
 
   if presets_exists and config.build_preset then
     args = { "--build", "--preset", config.build_preset } -- preset don't need define build dir.
+    cwd = config.cwd
   else
     args = {
       "--build",
       utils.transform_path(config:build_directory_path(), config.executor.name == "quickfix"),
     }
+    cwd = "."
   end
 
   vim.list_extend(args, config:build_options())
@@ -317,7 +320,7 @@ function cmake.build(opt, callback)
 
   local env = environment.get_build_environment(config)
   local cmd = const.cmake_command
-  return utils.execute(cmd, config.env_script, env, args, config.cwd, config.executor, function()
+  return utils.execute(cmd, config.env_script, env, args, cwd, config.executor, function()
     if type(callback) == "function" then
       callback()
     end
